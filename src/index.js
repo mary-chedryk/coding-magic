fb26611fd823f01bc25e7757091363706d232b22
Mariia Chedryk <mary.chedryk@gmail.com>
2025-03-01 15:38:41 -0500

    added files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import * as bootstrap from '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import './js/header.js';
+import './js/birthday.js';
+import './js/random.js';
+import './js/rock_paper_scissors.js';
+import './js/calculator.js';
+import './js/time_calculator.js';
+import './js/google_dino.js';
+import './js/football.js';
+import './js/numbers.js';
+import './js/team.js';
+import './js/scientist.js';
+import './js/footer.js';
