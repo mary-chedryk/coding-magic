38b05e47dae156dea3f7317d58c80e1a70d4a4f9
Martseniuk Alina <martseniuk.av@gmail.com>
2025-03-17 16:38:35 +0200

    add

--- src/js/numbers.js
+++ src/js/numbers.js
@@ -1,17 +1,37 @@
-function findMax() {
-    let a = document.getElementById("number1").value;
-    let b = document.getElementById("number2").value;
-    let c = document.getElementById("number3").value;
+// import { findMax } from "./js/three_numbers.js";
 
-    a = Number(a);
-    b = Number(b);
-    c = Number(c);
+// document.querySelectorAll(".num11, .num22, .num33").forEach(input => {
+//     input.addEventListener("input", findMax);
+// });
 
-    if (isNaN(a) || isNaN(b) || isNaN(c)) {
-        document.getElementById("numbers-result").textContent = "Будь ласка, введіть числа.";
-        return;
-    }
+// export function findMax() {
+//     let num1 = parseFloat(document.querySelector(".num11").value);
+//     let num2 = parseFloat(document.querySelector(".num22").value);
+//     let num3 = parseFloat(document.querySelector(".num33").value);
+//     let resultElement = document.querySelector(".result-one");
 
-    let maxNumber = Math.max(a, b, c);
-    document.getElementById("numbers-result").textContent = maxNumber;
-}
+//     if (!isNaN(num1) && !isNaN(num2) && !isNaN(num3)) {
+//         let maxNumber = Math.max(num1, num2, num3);
+//         resultElement.innerText = "Найбільше число, яке ви ввели - " + maxNumber;
+//     } else {
+//         resultElement.innerText = "Найбільше число, яке ви ввели - ${maxNumber}";
+//     }
+// }
+
+// function findMax() {
+//     let a = document.getElementById("number1").value;
+//     let b = document.getElementById("number2").value;
+//     let c = document.getElementById("number3").value;
+
+//     a = Number(a);
+//     b = Number(b);
+//     c = Number(c);
+
+//     if (isNaN(a) || isNaN(b) || isNaN(c)) {
+//         document.getElementById("numbers-result").textContent = "Будь ласка, введіть числа.";
+//         return;
+//     }
+
+//     let maxNumber = Math.max(a, b, c);
+//     document.getElementById("numbers-result").textContent = maxNumber;
+// }
